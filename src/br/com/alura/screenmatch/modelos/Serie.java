538a06733e7b184e5a910591b74dff953f875ce8
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-01-17 07:55:31 -0400

    Modelando o ScrennMatch

---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    @Override //sobrescrita de métodos - retorna algo da SUPERCLASSE de maneira diferente
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTemporada * minutosPorEpisodios;
+    }
+}
