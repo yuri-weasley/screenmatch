72ecbb9503de3921ac19a6a15892a5dd55e5b7bc
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-11-19 19:48:54 -0400

    Integração com a API do OMDb

--- src/br/com/alura/principal/PrincipalComListas.java
+++ src/br/com/alura/principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -20,7 +18,7 @@ public class PrincipalComListas {
 
         Filme f1 = filmeDoPaulo;
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(filmeDoPaulo);
         lista.add(meuFilme);
         lista.add(outroFilme);
